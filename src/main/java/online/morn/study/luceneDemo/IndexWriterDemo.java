739f3cbc6fc133b64d15e6c84346f41887f1f1b2
Horner <i@morn.online>
2018-06-02 14:46:50 +0800

    Initial commit

---
+++ src/main/java/online/morn/study/luceneDemo/IndexWriterDemo.java
@@ -0,0 +1,74 @@
+package online.morn.study.luceneDemo;
+
+import online.morn.study.luceneDemo.ik.IKAnalyzer4Lucene7;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.IndexOptions;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 写索引Demo
+ * @auther Horner 2018/5/18 0:10
+ */
+public class IndexWriterDemo implements Closeable {
+    private final static String FILE_PITH = "C:/Document/lucene-index";
+    private IndexWriter indexWriter;
+
+    public IndexWriterDemo() throws IOException {
+        Analyzer analyzer = new IKAnalyzer4Lucene7(true);//分词器
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);//索引配置
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);//设置索引库的打开模式：新建、追加、新建或追加
+        //索引存放目录
+        Directory directory = FSDirectory.open((new File(FILE_PITH)).toPath());//存放到文件系统中
+        //Directory directory = new RAMDirectory();//存放到内存中
+        this.indexWriter = new IndexWriter(directory, config);//创建索引写对象
+    }
+
+    public void addDocument(Document doc) throws IOException {
+        this.indexWriter.addDocument(doc);//将文档添加到索引
+        //this.indexWriter.deleteDocuments(terms);//删除文档
+        //this.indexWriter.updateDocument(term, doc);//修改文档
+        this.indexWriter.flush();//刷新
+        this.indexWriter.commit();//提交
+        //writer.rollback();//回滚
+    }
+
+    public static Field makeFiled(String fieldName, String fieldValue, boolean tokenized){
+        FieldType onlyStoredType = new FieldType();
+        onlyStoredType.setTokenized(tokenized);//是否分词
+        onlyStoredType.setIndexOptions(IndexOptions.DOCS);//索引选项：是否存储 词频、位置、偏移量
+        onlyStoredType.setStored(true);//是否存储
+        onlyStoredType.freeze();
+        return new Field(fieldName, fieldValue, onlyStoredType);
+    }
+
+    @Override
+    public void close() throws IOException {
+        indexWriter.close();//关闭 会自动提交
+    }
+
+    public static void main(String[] args) throws IOException {
+        IndexWriterDemo indexWriterDemo = new IndexWriterDemo();
+
+        Document doc = new Document();//创建document
+        doc.add(IndexWriterDemo.makeFiled("title","我叫小花啦啦啦",true));
+        doc.add(IndexWriterDemo.makeFiled("content","这就是最真实的数据啦",true));
+        doc.add(IndexWriterDemo.makeFiled("product","太阳能热水器",true));
+        indexWriterDemo.addDocument(doc);
+
+        Document doc2 = new Document();//创建document
+        doc2.add(IndexWriterDemo.makeFiled("title","我叫小花啦啦啦2",true));
+        doc2.add(IndexWriterDemo.makeFiled("content","这就是最真实的数据啦2",true));
+        doc2.add(IndexWriterDemo.makeFiled("product","太阳能热水器2",true));
+        indexWriterDemo.addDocument(doc2);
+    }
+}
+
+
---
+++ src/main/java/online/morn/study/luceneDemo/ik/IKTokenizer4Lucene7Factory.java
@@ -0,0 +1,26 @@
+package online.morn.study.luceneDemo.ik;
+
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.util.TokenizerFactory;
+import org.apache.lucene.util.AttributeFactory;
+
+import java.util.Map;
+
+public class IKTokenizer4Lucene7Factory extends TokenizerFactory {
+
+	private boolean useSmart = false;
+
+	public IKTokenizer4Lucene7Factory(Map<String, String> args) {
+		super(args);
+		String useSmartParm = args.get("useSmart");
+		if ("true".equalsIgnoreCase(useSmartParm)) {
+			this.useSmart = true;
+		}
+	}
+
+	@Override
+	public Tokenizer create(AttributeFactory factory) {
+		return new IKTokenizer4Lucene7(this.useSmart);
+	}
+
+}
